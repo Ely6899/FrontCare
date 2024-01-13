2c7088c0cc3d2d2e172e5eef049dc00f75d04591
Raz-Saad <43138073+Raz-Saad@users.noreply.github.com>
2024-01-13 17:18:32 +0200

    Add files via upload
    updated DB

---
+++ Database/frontcare.sql
@@ -0,0 +1,205 @@
+CREATE DATABASE  IF NOT EXISTS `frontcare` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `frontcare`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: frontcare
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `donation_events`
+--
+
+DROP TABLE IF EXISTS `donation_events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `donation_events` (
+  `event_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `donor_id` int unsigned NOT NULL,
+  `event_date` date NOT NULL,
+  `event_location` varchar(45) NOT NULL,
+  `event_address` varchar(45) NOT NULL,
+  `remaining_spot` int unsigned NOT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `donation_events`
+--
+
+LOCK TABLES `donation_events` WRITE;
+/*!40000 ALTER TABLE `donation_events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `donation_events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event_details`
+--
+
+DROP TABLE IF EXISTS `event_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `event_details` (
+  `event_id` int unsigned NOT NULL,
+  `product_id` int unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event_details`
+--
+
+LOCK TABLES `event_details` WRITE;
+/*!40000 ALTER TABLE `event_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event_participants`
+--
+
+DROP TABLE IF EXISTS `event_participants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `event_participants` (
+  `event_id` int unsigned NOT NULL,
+  `user_id` int unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event_participants`
+--
+
+LOCK TABLES `event_participants` WRITE;
+/*!40000 ALTER TABLE `event_participants` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_participants` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `product_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `request_details`
+--
+
+DROP TABLE IF EXISTS `request_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `request_details` (
+  `request_id` int unsigned NOT NULL,
+  `product_id` int unsigned NOT NULL,
+  `quantity` int unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `request_details`
+--
+
+LOCK TABLES `request_details` WRITE;
+/*!40000 ALTER TABLE `request_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `request_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `soldier_requests`
+--
+
+DROP TABLE IF EXISTS `soldier_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `soldier_requests` (
+  `request_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `soldier_id` int unsigned NOT NULL,
+  `donor_id` int unsigned DEFAULT NULL,
+  `pickup_location` varchar(45) NOT NULL,
+  `request_date` date NOT NULL,
+  `close_date` date DEFAULT NULL,
+  `status` int unsigned NOT NULL,
+  PRIMARY KEY (`request_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `soldier_requests`
+--
+
+LOCK TABLES `soldier_requests` WRITE;
+/*!40000 ALTER TABLE `soldier_requests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `soldier_requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `is_solider` binary(1) DEFAULT NULL,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `email_address` varchar(45) NOT NULL,
+  `phone_number` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-13 17:16:15
