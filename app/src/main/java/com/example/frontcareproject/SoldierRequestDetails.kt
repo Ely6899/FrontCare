37664c997451d05420c2373b447ff76f9b0a69e2
Idan <idanyanai22@gmail.com>
2024-01-18 20:58:28 +0200

    SoldierRequestDetails activity shows info now

--- app/src/main/java/com/example/frontcareproject/SoldierRequestDetails.kt
+++ app/src/main/java/com/example/frontcareproject/SoldierRequestDetails.kt
@@ -1,15 +1,23 @@
 package com.example.frontcareproject
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONArray
 import android.util.Log;
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.TableLayout
+import android.widget.TableRow
 import org.json.JSONObject
 
 
 class SoldierRequestDetails : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_soldier_request_details)
@@ -24,32 +32,57 @@ class SoldierRequestDetails : AppCompatActivity() {
             }
         }
 
-        // Check if the array is not empty
-        if (jsonArray.length() > 0) {
-            // Get the first JSON object from the array
-            val jsonObject = jsonArray.getJSONObject(0)
+        // Get the first JSON object from the array
+        val jsonObject = jsonArray.getJSONObject(0)
 
-            // Retrieve data from the JSON object
-            val requestDate = jsonObject.getString("request_date")
-            val firstname = jsonObject.getString("firstname")
-            val productName = jsonObject.getString("product_name")
-            val quantity = jsonObject.getInt("quantity").toString()
-            val pickupLocation = jsonObject.getString("pickup_location")
+        // Retrieve data from the JSON object
+        val requestDate = jsonObject.getString("request_date")
+        val firstname = jsonObject.getString("firstname")
+        val pickupLocation = jsonObject.getString("pickup_location")
 
-            Log.d("SoldierRequestDetails", "------------------------------- $requestDate -------------------")
-
-            // Use the data to populate UI elements in SoldierRequestDetails activity
-            val requestDateTextView: TextView = findViewById(R.id.requestDateTextView)
-            val firstnameTextView: TextView = findViewById(R.id.firstnameTextView)
-            val productNameTextView: TextView = findViewById(R.id.productNameTextView)
-            val quantityTextView: TextView = findViewById(R.id.quantityTextView)
-            val pickupLocationTextView: TextView = findViewById(R.id.pickupLocationTextView)
-
-            requestDateTextView.text = "Request Date: $requestDate"
-            firstnameTextView.text = "First Name: $firstname"
-            productNameTextView.text = "Product Name: $productName"
-            quantityTextView.text = "Quantity: $quantity"
-            pickupLocationTextView.text = "Pickup Location: $pickupLocation"
+        // Get products and put them in map
+        var products = mutableMapOf<String, Int>()
+        for (i in 0 until jsonArray.length()) {
+            val obj = jsonArray.getJSONObject(i)
+            products[obj.getString("product_name")] = obj.getInt("quantity")
         }
+
+        val productsTableView: TableLayout = findViewById(R.id.productsTable)
+
+        for ((product, quantity) in products) {
+            val newRow = TableRow(this)
+
+            val productView = createTextView(product)
+            val quantityView = createTextView("$quantity")
+
+            newRow.addView(productView)
+            newRow.addView(quantityView)
+
+            productsTableView.addView(newRow)
+
+        }
+
+        // Use the data to populate UI elements in SoldierRequestDetails activity
+        val requestDateTextView: TextView = findViewById(R.id.requestDateTextView)
+        val firstnameTextView: TextView = findViewById(R.id.firstnameTextView)
+        val pickupLocationTextView: TextView = findViewById(R.id.pickupLocationTextView)
+
+        requestDateTextView.text = "Request Date: $requestDate"
+        firstnameTextView.text = "First Name: $firstname"
+        pickupLocationTextView.text = "Pickup Location: $pickupLocation"
+    }
+
+    private fun createTextView(text: String): TextView {
+        val textView = TextView(this)
+        textView.text = text
+        textView.gravity = Gravity.CENTER
+        textView.setPadding(8, 8, 8, 8)
+
+        val newLayoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1.0f)
+        newLayoutParams.gravity = Gravity.CENTER
+
+        textView.layoutParams = newLayoutParams
+
+        return textView
     }
 }
