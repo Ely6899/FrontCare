09443b9c169507d5abeee5e9f8e13fa19c25436a
Idan <idanyanai22@gmail.com>
2024-01-19 00:49:30 +0200

    Donate button now send HTTP request to server

--- app/src/main/java/com/example/frontcareproject/SoldierRequestDetails.kt
+++ app/src/main/java/com/example/frontcareproject/SoldierRequestDetails.kt
@@ -5,14 +5,18 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONArray
-import android.util.Log;
 import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.ListView
+import android.view.View
+import android.widget.Button
 import android.widget.TableLayout
 import android.widget.TableRow
 import org.json.JSONObject
+import server.userId
+import utils.GlobalVar
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 
 class SoldierRequestDetails : AppCompatActivity() {
@@ -26,6 +30,7 @@ class SoldierRequestDetails : AppCompatActivity() {
         val jsonStringList = intent.getStringArrayListExtra("jsonArray")
         val jsonArray = JSONArray()
 
+        // Cast back into JSONArray
         if (jsonStringList != null) {
             for (jsonString in jsonStringList) {
                 jsonArray.put(JSONObject(jsonString))
@@ -36,9 +41,25 @@ class SoldierRequestDetails : AppCompatActivity() {
         val jsonObject = jsonArray.getJSONObject(0)
 
         // Retrieve data from the JSON object
+        val requestId = jsonObject.getInt("request_id")
         val requestDate = jsonObject.getString("request_date")
         val firstname = jsonObject.getString("firstname")
         val pickupLocation = jsonObject.getString("pickup_location")
+
+        // Get views
+        val requestDateTextView: TextView = findViewById(R.id.requestDateTextView)
+        val firstnameTextView: TextView = findViewById(R.id.firstnameTextView)
+        val pickupLocationTextView: TextView = findViewById(R.id.pickupLocationTextView)
+        val donateButtonView: Button = findViewById(R.id.donateButton)
+        val productsTableView: TableLayout = findViewById(R.id.productsTable)
+
+        // Add button listener
+        donateButtonView.setOnClickListener(donate(requestId))
+
+        // Display user data
+        requestDateTextView.text = "Request Date: $requestDate"
+        firstnameTextView.text = "First Name: $firstname"
+        pickupLocationTextView.text = "Pickup Location: $pickupLocation"
 
         // Get products and put them in map
         var products = mutableMapOf<String, Int>()
@@ -47,37 +68,68 @@ class SoldierRequestDetails : AppCompatActivity() {
             products[obj.getString("product_name")] = obj.getInt("quantity")
         }
 
-        val productsTableView: TableLayout = findViewById(R.id.productsTable)
+        // Display products in table
+        displayProducts(productsTableView, products)
+    }
 
+    private fun donate(requestId: Int): View.OnClickListener {
+        return View.OnClickListener {view ->
+            //if you are using your phone instead of emulator you need to change the ip
+            val url = URL("http://${GlobalVar.serverIP}:8080/api/donation")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            connection.setRequestProperty("Content-Type", "application/json")
+            connection.doOutput = true
+
+            // Construct the JSON payload with email and password
+            val jsonInputString = """{"userId": "$userId", "requestId": "$requestId"}"""
+
+            // Send JSON as the request body
+            val outputStream = connection.outputStream
+            outputStream.write(jsonInputString.toByteArray(Charsets.UTF_8))
+            outputStream.close()
+
+            // Get the response
+            val inputStream = connection.inputStream
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val serverAns = reader.readLine()
+
+            // check for errors
+            val responseCode = connection.responseCode
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                // Do something
+            }
+
+            reader.close()
+            connection.disconnect()
+        }
+    }
+
+    private fun displayProducts(productsTableView: TableLayout, products: Map<String, Int>) {
+        // Go over each item in products map
         for ((product, quantity) in products) {
+            // create new row
             val newRow = TableRow(this)
 
+            // Create text views
             val productView = createTextView(product)
             val quantityView = createTextView("$quantity")
 
+            // Add text views to the row
             newRow.addView(productView)
             newRow.addView(quantityView)
 
+            // Add the row to the table
             productsTableView.addView(newRow)
-
         }
-
-        // Use the data to populate UI elements in SoldierRequestDetails activity
-        val requestDateTextView: TextView = findViewById(R.id.requestDateTextView)
-        val firstnameTextView: TextView = findViewById(R.id.firstnameTextView)
-        val pickupLocationTextView: TextView = findViewById(R.id.pickupLocationTextView)
-
-        requestDateTextView.text = "Request Date: $requestDate"
-        firstnameTextView.text = "First Name: $firstname"
-        pickupLocationTextView.text = "Pickup Location: $pickupLocation"
     }
-
     private fun createTextView(text: String): TextView {
         val textView = TextView(this)
         textView.text = text
         textView.gravity = Gravity.CENTER
         textView.setPadding(8, 8, 8, 8)
 
+        // Make the text views take up the whole table
         val newLayoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1.0f)
         newLayoutParams.gravity = Gravity.CENTER
 
