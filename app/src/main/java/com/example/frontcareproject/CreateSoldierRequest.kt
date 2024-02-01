5197cc401d70f9c8bf8d07d63bbd15c2cc8675f6
Idan <idanyanai22@gmail.com>
2024-02-01 15:05:46 +0200

    Changed CreateSoldierRequest to use a spinner for products.

--- app/src/main/java/com/example/frontcareproject/CreateSoldierRequest.kt
+++ app/src/main/java/com/example/frontcareproject/CreateSoldierRequest.kt
@@ -3,10 +3,12 @@ package com.example.frontcareproject
 import android.content.Intent
 import android.os.Bundle
 import android.view.Gravity
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Spinner
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
@@ -20,40 +22,91 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 import utils.GlobalVar
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class CreateSoldierRequest : AppCompatActivity() {
-    private lateinit var productNameEditView : EditText
+    private lateinit var productsSpinnerView : Spinner
     private lateinit var  quantityEditView : EditText
     private lateinit var  addProductButton : Button
     private lateinit var  createButton : Button
     private lateinit var  productsTableView : TableLayout
     private lateinit var  radioGroup : RadioGroup
 
-    private val products = mutableMapOf<String, Int>(); // create new map to save table entries
+    private var availableProducts : MutableMap<String, String> = mutableMapOf() // create new map to save table entries
+    private var productsToSend : MutableMap<Int, Int> = mutableMapOf() // Products in the table
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_soldier_request)
 
-        productNameEditView = findViewById(R.id.productName);
-        quantityEditView = findViewById(R.id.quantity);
-        addProductButton = findViewById(R.id.addToListButton);
-        createButton = findViewById(R.id.createButton);
-        productsTableView = findViewById(R.id.productsTable);
-        radioGroup = findViewById(R.id.radioGroup);
+        productsSpinnerView = findViewById(R.id.productsSpinner)
+        quantityEditView = findViewById(R.id.quantity)
+        addProductButton = findViewById(R.id.addToListButton)
+        createButton = findViewById(R.id.createButton)
+        productsTableView = findViewById(R.id.productsTable)
+        radioGroup = findViewById(R.id.radioGroup)
+
+        // Get availableProducts from server
+        getProducts()
 
         // Add button even listener
         addProductButton.setOnClickListener {
-            onAddProductButtonClick(products)
+            onAddProductButtonClick()
         }
 
         // Create button event listener
         createButton.setOnClickListener{
-            onCreateButtonClick(products)
+            onCreateButtonClick()
         }
     }
 
-    private fun onCreateButtonClick(products: MutableMap<String, Int>) {
+    private fun getProducts() {
+        val url = "http://${GlobalVar.serverIP}:8080/api/products"
+
+        //var products: MutableMap<String, String> = mutableMapOf()
+        val gson = Gson()
+        val type = object : TypeToken<MutableMap<String, String>>() {}.type
+
+        // Request in a new Coroutine that is destroyed after leaving this scope
+        lifecycleScope.launch(Dispatchers.IO) {
+            val client = OkHttpClient()
+
+            // build the request
+            val request = Request.Builder()
+                .url(url)
+                .build()
+
+            // send request and put response in variable
+            val response = client.newCall(request).execute()
+
+            // check response code
+            if (response.isSuccessful) {
+                // need to run on new thread because we are changing the UI
+                runOnUiThread {
+                    // transform json back to map
+                    availableProducts = gson.fromJson(response.body?.string(), type)
+
+                    // use adapter to put the items in the spinner
+                    val adapter = ArrayAdapter(
+                        this@CreateSoldierRequest,
+                        android.R.layout.simple_spinner_item,
+                        availableProducts.values.toList() // take only the names of the products.
+                    )
+
+                    // Specify the layout to use when the list of choices appears
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+                    // Apply the adapter to the spinner
+                    productsSpinnerView.adapter = adapter
+                }
+            } else {
+                println("Request failed with code: ${response.code}")
+            }
+        }
+    }
+
+    private fun onCreateButtonClick() {
         // If none radio button is selected return
         val selectedRadioButtonId = radioGroup.checkedRadioButtonId
         if (selectedRadioButtonId == -1) {
@@ -70,7 +123,7 @@ class CreateSoldierRequest : AppCompatActivity() {
 
         // convert Products map to jsonArray
         val gson = Gson()
-        val jsonProducts = gson.toJson(products)
+        val jsonProducts = gson.toJson(productsToSend)
 
         // Request in a new Coroutine that is destroyed after leaving this scope
         lifecycleScope.launch(Dispatchers.IO) {
@@ -101,13 +154,17 @@ class CreateSoldierRequest : AppCompatActivity() {
         }
     }
 
-    private fun onAddProductButtonClick(products : MutableMap<String, Int>) {
-        val productName = productNameEditView.text.toString().trim();
+    private fun onAddProductButtonClick() {
+        val productName = productsSpinnerView.selectedItem.toString()
         val quantity = quantityEditView.text.toString().toInt()
 
         if (productName.isNotEmpty() && quantity >= 0) {
+            // find product ID by name.
+            val productId = availableProducts.entries.find { it.value == productName }?.key?.toInt()
+
             // Put in map
-            products[productName] = quantity
+            if (productId != null)
+                productsToSend[productId] = quantity
 
             // create new table row
             val newRow = TableRow(this)
@@ -125,7 +182,6 @@ class CreateSoldierRequest : AppCompatActivity() {
 
             // Clear edit texts
             quantityEditView.text.clear()
-            productNameEditView.text.clear()
         }
     }
 
